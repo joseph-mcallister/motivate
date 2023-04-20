a00f0cde747ffb82c50181ac929031d9102cdd90
Joseph McAllister <joseph.l.mcallister@gmail.com>
2023-04-19 22:09:51 -0400

    initial commit for flask

--- flask-app/application.py
+++ flask-app/application.py
@@ -2,8 +2,12 @@ import os
 from typing import Dict
 from aiohttp_retry import Any
 from flask import Flask, request
+from langchain import PromptTemplate
 from twilio.twiml.messaging_response import MessagingResponse
 import json
+
+from langchain.llms import OpenAI
+from langchain.memory import ConversationSummaryBufferMemory
 
 from supabase import create_client, Client
 from twilio_utils import validate_twilio_request
@@ -30,7 +34,7 @@ def twilio_callback():
         if len(user_res) == 0:
             print("user")
             return "user not found", 200
-        sender_user_name = user_res[0]["name"]
+        sender_name = user_res[0]["name"]
         sender_user_id = user_res[0]["id"]
 
         group_member_res = supabase.table("group_members").select("*").eq("twilio_phone_number", reciever).eq("user_id", sender_user_id).execute().data
@@ -43,10 +47,10 @@ def twilio_callback():
             return "group not found", 200
         group = group_res[0]
 
-        llm_message = get_llm_message(group, incoming_msg)
+        llm_message = get_llm_message(group, incoming_msg, sender_name)
 
         resp = MessagingResponse()
-        res  = f"Test, {sender}, {incoming_msg}, {sender_user_name}, {group_id}, {llm_message}"
+        res  = f"{llm_message}"
         resp.message(res)
 
         return str(resp)
@@ -55,9 +59,20 @@ def twilio_callback():
         print(e)
         return "error", 500
 
+llm = OpenAI()
+ai_prompt = PromptTemplate(input_variables=[], template="AI Motvator: ")
+human_prompt = PromptTemplate(input_variables=["name", "msg"], template="{name}: {msg}")
 
-def get_llm_message(group: Dict[str, Any], incoming_msg: str):
-    return "test"
+def get_llm_message(group: Dict[str, Any], incoming_msg: str, sender_name: str):
+    if group["memory"] == None:
+        initial_prompt = (
+            group["base_prompt"] 
+            + "\n" 
+            + group["initial_context"] 
+            + human_prompt.format(name=sender_name, msg=incoming_msg)
+            + ai_prompt.format()
+        )
+        return llm(initial_prompt).strip()
 
 if __name__ == "__main__":
     application.debug = True
